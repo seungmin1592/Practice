e1e02799b507387bd9d4707d5acc0fd71ff44e06
seungmin1592 <tmdals1592@naver.com>
2021-06-30 18:14:54 +0900

    Create Main.java

---
+++ Java/210630_Java_Exception/src/com/ex/Main.java
@@ -0,0 +1,15 @@
+package com.ex;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int i = 10;
+		int j = 0;
+		
+		System.out.println(i*j);
+		System.out.println(i/j);
+		System.out.println(i+j);
+		System.out.println(i-j);
+	}
+
+}
