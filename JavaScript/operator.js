8387c3784c4d60be1f603c2b2cebf6bfa25407b7
seungmin1592 <tmdals1592@naver.com>
2021-07-07 22:58:08 +0900

    JavaScript_Operator
    JavaScript_Practice
    
    Operator

---
+++ JavaScript/operator.js
@@ -0,0 +1,28 @@
+// 1. String concatenation
+console.log('my' + ' cat');
+console.log('1' + 2);
+console.log(`string literals : 1 + 2 = ${1 + 2}`);
+
+// 2. Numeric operators
+console.log(1 + 1);
+console.log(1 - 1);
+console.log(1 * 1);
+console.log(1 / 1);
+console.log(1 % 1);
+console.log(1 ** 1);
+
+
+// 3. Increment and decrement operators
+let counter = 2;
+const preIncrement = ++counter;
+// counter = counter + 1;
+// preIncrement = counter;
+
+
+
+// 4. Assingnment operators
+let x = 3;
+let y = 6;
+// x += y -> x = x + y;
+
+
