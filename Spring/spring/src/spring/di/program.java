e363912d98e2229ad0fbc90da594a8ae96a0a152
seungmin1592 <tmdals1592@naver.com>
2021-08-16 16:02:16 +0900

    Spring Autowired

--- Spring/spring/src/spring/di/program.java
+++ Spring/spring/src/spring/di/program.java
@@ -29,19 +29,21 @@ public class program {
 		ApplicationContext context = 
 				new ClassPathXmlApplicationContext("spring/di/setting.xml");
 		
-		Exam exam = context.getBean(Exam.class);
-		System.out.println(exam.toString());
-		// ExamConsole console = (ExamConsole) context.getBean("console");
-		ExamConsole console = context.getBean(ExamConsole.class);
+		//Exam exam = context.getBean(Exam.class);
+		//System.out.println(exam.toString());
+		
+		
+		ExamConsole console = (ExamConsole) context.getBean("console");
+		// ExamConsole console = context.getBean(ExamConsole.class);
 		
 		console.print();
 		
-		List<Exam> exams = (List<Exam>) context.getBean("exams"); // new ArrayList<>();
-		exams.add(new NewlecExam(1,1,1,1));
-		
-		
-		for(Exam e: exams)
-			System.out.println(e);
+//		List<Exam> exams = (List<Exam>) context.getBean("exams"); // new ArrayList<>();
+//		exams.add(new NewlecExam(1,1,1,1));
+//		
+//		
+//		for(Exam e: exams)
+//			System.out.println(e);
 	}
 
 }
--- Spring/spring/src/spring/di/ui/InlineExamConsole.java
+++ Spring/spring/src/spring/di/ui/InlineExamConsole.java
@@ -1,26 +1,38 @@
 package spring.di.ui;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+
 import spring.di.entity.Exam;
 
 public class InlineExamConsole implements ExamConsole {
 	
+	@Autowired(required = false)
+	@Qualifier("exam1")
 	private Exam exam;
 
 	public InlineExamConsole() {
-		// TODO Auto-generated constructor stub
+		System.out.println("constructor");
 	}
 	
+	
 	public InlineExamConsole(Exam exam) {
+		System.out.println("overloaded constructor");
 		this.exam = exam;
 	}
 
 	@Override
 	public void print() {
-		System.out.printf("total is %d, avg is %f\n", exam.total(), exam.avg());
+		if(exam == null)
+			System.out.printf("total is %d, avg is %f\n",0, 0.0);
+		else 
+			System.out.printf("total is %d, avg is %f\n", exam.total(), exam.avg());
 	}
 
+	
 	@Override
 	public void setExam(Exam exam) {
+		System.out.println("setter injection");
 		this.exam = exam;		
 	}
 
