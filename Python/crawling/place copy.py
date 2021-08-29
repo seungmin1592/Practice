a7901e9c825e904379de341d989b61b901e6e81d
seungmin1592 <tmdals1592@naver.com>
2021-08-29 23:34:36 +0900

    csv 저장

--- Python/crawling/place copy.py
+++ Python/crawling/place copy.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from bs4 import BeautifulSoup
-import time 
+import time
 import urllib.request
 import re
 
@@ -10,7 +10,7 @@ options = webdriver.ChromeOptions()
 options.add_experimental_option("excludeSwitches", ["enable-logging"])
 
 driver = webdriver.Chrome(
-    options=options, executable_path="chromedriver")
+    options=options, executable_path=r"C:\Users\User\Documents\GitHub\Practice\Python\selenium\chromedriver.exe")
 driver.get("https://map.naver.com/v5/search")
 
 
@@ -39,17 +39,17 @@ driver.switch_to.frame('searchIframe')
 
 time.sleep(2)
 #page_num = driver.find_element_by_css_selector('.place_didmount ._2ky45 ._2tk2s:nth-child(' + str(page) + ')')
-pages_num = driver.find_elements_by_css_selector('.place_didmount ._2ky45 ._2tk2s')
+pages_num = driver.find_elements_by_css_selector(
+    '.place_didmount ._2ky45 ._2tk2s')
 print(pages_num)
 
 # list li의 idx값
 i = 0
 print('------------- i의 값 : ' + str(i))
 
-for page in pages_num :
+for page in pages_num:
     page.click()
     print('page === ' + str(page))
-
 
     list_idx = 0
 
@@ -59,33 +59,36 @@ for page in pages_num :
         # searchiframe으로 전환
         driver.switch_to.default_content()
         driver.switch_to.frame('searchIframe')
-            
+
         # list li의 idx값 1씩 증가
         i += 1
         print('=================================================================')
-        print('=============================== '+ str(i) + '번 =============================')
+        print('=============================== ' +
+              str(i) + '번 =============================')
 
         list_idx += 1
 
         # 한 페이지에 클릭할 place가 없을 경우 break를 하기 위한 체크 값
-        try :
-            list_ldx_chk = bool(driver.find_element_by_css_selector('#_pcmap_list_scroll_container ul li:nth-child(' + str(list_idx) + ')'))
+        try:
+            list_ldx_chk = bool(driver.find_element_by_css_selector(
+                '#_pcmap_list_scroll_container ul li:nth-child(' + str(list_idx) + ')'))
             print('list 여부 : ' + str(list_ldx_chk))
 
             # 클락할 place 리스트가 있을 경우
-            if list_ldx_chk :
+            if list_ldx_chk:
 
                 # 광고 list 제외하기 위한 체크 값
-                advertis_chk = bool(driver.find_elements_by_css_selector('#_pcmap_list_scroll_container ul li:not(._293u6):nth-child(' + str(list_idx) + ')'))
-
+                advertis_chk = bool(driver.find_elements_by_css_selector(
+                    '#_pcmap_list_scroll_container ul li:not(._293u6):nth-child(' + str(list_idx) + ')'))
 
                 # 광고 list에 붙는 class명이 없을 경우(광고가 아닐 경우)
-                if advertis_chk :
-                    
+                if advertis_chk:
+
                     print('광고가 아닌지 여부 : ' + str(advertis_chk))
 
                     # place list 클릭해서 detailpage open
-                    content = driver.find_element_by_css_selector('#_pcmap_list_scroll_container ul li:nth-child(' + str(list_idx) + ') ._3ZU00 ._3LMxZ:first-child')
+                    content = driver.find_element_by_css_selector(
+                        '#_pcmap_list_scroll_container ul li:nth-child(' + str(list_idx) + ') ._3ZU00 ._3LMxZ:first-child')
                     content.click()
                     time.sleep(3)
 
@@ -97,56 +100,57 @@ for page in pages_num :
                     html = driver.page_source
                     soup = BeautifulSoup(html, "html.parser")
 
-
                     # ========== place 정보 추출 ==============
-
 
                     # place 이름
                     place_name = soup.select_one('#_title ._3XamX').text
-                    print('매장명 : ' + place_name)    
+                    print('매장명 : ' + place_name)
 
                     # place 전화번호
-                    try :
-                        place_tel = soup.select_one('.place_section_content ._3xPmJ ._1h3B_ ._3ZA0S').text
-                        print('전화번호 : ' + place_tel) 
-                    except Exception as e :
+                    try:
+                        place_tel = soup.select_one(
+                            '.place_section_content ._3xPmJ ._1h3B_ ._3ZA0S').text
+                        print('전화번호 : ' + place_tel)
+                    except Exception as e:
                         place_tel = ''
                         print('전화번호 : ' + place_tel)
 
                     # place 위치
-                    place_address = soup.select_one('.place_section_content ._1aj6- ._1h3B_ ._2yqUQ').text
-                    print('주소 : ' + place_address) 
+                    place_address = soup.select_one(
+                        '.place_section_content ._1aj6- ._1h3B_ ._2yqUQ').text
+                    print('주소 : ' + place_address)
 
                     # place 소개
-                    try :
-                        place_introduce_open = driver.find_element_by_css_selector('.place_section_content ._3__3i ._1h3B_ .M_704')
+                    try:
+                        place_introduce_open = driver.find_element_by_css_selector(
+                            '.place_section_content ._3__3i ._1h3B_ .M_704')
                         place_introduce_open.click()
                         time.sleep(2)
 
-                        # 내용 더 보기를 클릭함으로 해당 클래스 안에 text가 더 노출되며 동시에 해당 클래스에 클래스명이 추가된다. 
-                        # 추가된 클래스명은 html변수에 driver.page_source로 담겨있지 않기 때문에 새롭게 선언해준다. 
+                        # 내용 더 보기를 클릭함으로 해당 클래스 안에 text가 더 노출되며 동시에 해당 클래스에 클래스명이 추가된다.
+                        # 추가된 클래스명은 html변수에 driver.page_source로 담겨있지 않기 때문에 새롭게 선언해준다.
                         driver.switch_to.default_content()
                         driver.switch_to.frame('entryIframe')
                         # detail page의 html
                         html = driver.page_source
                         soup = BeautifulSoup(html, "html.parser")
 
-                                    
-                        place_introduce = soup.select_one('.place_section_content ._3__3i ._1h3B_ .M_704 .WoYOw').text
-                        print('소개 : ' + place_introduce) 
-                    except Exception as e :
+                        place_introduce = soup.select_one(
+                            '.place_section_content ._3__3i ._1h3B_ .M_704 .WoYOw').text
+                        print('소개 : ' + place_introduce)
+                    except Exception as e:
                         place_introduce = ""
                         print('소개 : ' + place_introduce)
 
-
                     # place 운영시간
-                    try :
-                        place_introduce_open = driver.find_element_by_css_selector('.place_section_content ._2KHqk ._1h3B_ ._2BDci') 
+                    try:
+                        place_introduce_open = driver.find_element_by_css_selector(
+                            '.place_section_content ._2KHqk ._1h3B_ ._2BDci')
                         place_introduce_open.click()
                         time.sleep(0.5)
-                        
-                        # 내용 더 보기를 클릭함으로 해당 클래스 안에 text가 더 노출되며 동시에 해당 클래스에 클래스명이 추가된다. 
-                        # 추가된 클래스명은 html변수에 driver.page_source로 담겨있지 않기 때문에 새롭게 선언해준다. 
+
+                        # 내용 더 보기를 클릭함으로 해당 클래스 안에 text가 더 노출되며 동시에 해당 클래스에 클래스명이 추가된다.
+                        # 추가된 클래스명은 html변수에 driver.page_source로 담겨있지 않기 때문에 새롭게 선언해준다.
                         driver.switch_to.default_content()
                         driver.switch_to.frame('entryIframe')
                         # detail page의 html
@@ -154,33 +158,35 @@ for page in pages_num :
                         soup = BeautifulSoup(html, "html.parser")
 
                         # 운영시간이 여러개로 입력되어 있을 경우
-                        try :
-                            place_time_lists = driver.find_elements_by_css_selector('.place_section_content ._2KHqk ._1h3B_ ._2BDci._1OkoP ._2ZP3j ._20pEw')
+                        try:
+                            place_time_lists = driver.find_elements_by_css_selector(
+                                '.place_section_content ._2KHqk ._1h3B_ ._2BDci._1OkoP ._2ZP3j ._20pEw')
 
                         # 운영시간이 힌개만 입력되어 있을 경우
-                        except Exception as e :
-                            place_time_lists = driver.find_elements_by_css_selector('.place_section_content ._2KHqk ._1h3B_ ._2BDci._1OkoP ._2ZP3j')
+                        except Exception as e:
+                            place_time_lists = driver.find_elements_by_css_selector(
+                                '.place_section_content ._2KHqk ._1h3B_ ._2BDci._1OkoP ._2ZP3j')
 
-                        for place_time_list in place_time_lists :
-                            print('영업시간 : ' + place_time_list.text) 
-                    except Exception as e :
+                        for place_time_list in place_time_lists:
+                            print('영업시간 : ' + place_time_list.text)
+                    except Exception as e:
                         print('영업시간 : ' + '')
 
                     # place 이용정보
-                    try :
-                        place_info = soup.select_one('.place_section_content li.undefined:last-child ._1h3B_').text
-                        print('이용정보 : ' + place_info) 
-                    except Exception as e :
+                    try:
+                        place_info = soup.select_one(
+                            '.place_section_content li.undefined:last-child ._1h3B_').text
+                        print('이용정보 : ' + place_info)
+                    except Exception as e:
                         place_info = ""
                         print('이용정보 :' + place_info)
-
 
                     # place image
                     # try :
                     #     image_section = driver.find_element_by_css_selector('.place_detail_wrapper ._2MDmw ._tab-menu:last-child')
 
                     #     image_section.click()
-                        
+
                     #     #시간 지연
                     #     time.sleep(3)
 
@@ -202,17 +208,12 @@ for page in pages_num :
                     # =========== place 정보 추출 완료 ============
 
                 # 광고일 경우 continue로 패스
-                else :
+                else:
                     continue
-                
+
                 # while문으로 다시 진행
                 continue
 
         # 다음 선택할 list가 없을 경우 while 종료
-        except Exception as e :    
+        except Exception as e:
             break
-        
-
-
-
-
--- Python/selenium/crawling.py
+++ Python/selenium/crawling.py
@@ -1,46 +1,276 @@
+from os import write
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-#from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup
 import time
+import csv
 import urllib.request
+import re
 
-driver = webdriver.Chrome("./chromedriver")
+# csv 파일로 저장
+file = open('place.csv', 'w', encoding='utf-8', newline='')
+writer = csv.writer(file)
+writer.writerow(('번호', '매장명', '위치', '전화번호', '이용정보', '소개', '영업시간',  '이미지'))
+
+
+# 크롬 웹드라이버 연결
+options = webdriver.ChromeOptions()
+options.add_experimental_option("excludeSwitches", ["enable-logging"])
+
+driver = webdriver.Chrome(
+    options=options, executable_path=r"C:\Users\User\Documents\GitHub\Practice\Python\selenium\chromedriver.exe")
 driver.get("https://map.naver.com/v5/search")
 
+
+# 시간 지연
 time.sleep(2)
+
+# 검색입력
 elem = driver.find_element_by_css_selector(
     ".panel_wrap .search_box .input_box .input_search")
-
 elem.send_keys("마포구 헬스장")
 elem.send_keys(Keys.RETURN)
 
-for p in range(20):
-    # 5초 delay
-    time.sleep(2)
 
-    js_script = "document.querySelector(\"body > app > layout > div > div.container > div.router-output > "\
-                "shrinkable-layout > search-layout > search-list > search-list-contents > perfect-scrollbar\").innerHTML"
-    raw = driver.execute_script("return " + js_script)
-
-    #html = BeautifulSoup(raw, "html.parser")
+# 5초 delay
+time.sleep(2)
 
 
-#images = driver.find_elements_by_css_selector(".rg_i.Q4LuWd")
-
-# count = 1
-# for image in images:
-#     image.click()
-#     time.sleep(3)
-#     imgUrl = driver.find_element_by_css_selector(
-#         ".n3VNCb").get_attribute("src")
-#     urllib.request.urlretrieve(imgUrl, str(count) + ".jpg")
-#     count = count + 1
+# 처음 화면의 html
+html = driver.page_source
+soup = BeautifulSoup(html, "html.parser")
 
 
-# assert "Python" in driver.title
-# elem = driver.find_element_by_name("q")
-# elem.clear()
-# elem.send_keys("pycon")
-# elem.send_keys(Keys.RETURN)
-# assert "No results found." not in driver.page_source
-# driver.close()
+driver.switch_to.frame('searchIframe')
+
+time.sleep(2)
+pages_num = driver.find_elements_by_css_selector(
+    '.place_didmount ._2ky45 ._2tk2s')
+print('pages_num = ' + str(len(pages_num)))
+
+# list li의 idx값
+i = 0
+
+
+# 5페이지까지의 데이터를 추출
+for page in pages_num:
+
+    try:
+        page.click()
+
+        # 페이징내에서의 리스트 idx값
+        list_idx = 0
+
+        # csv 파일로 저장할 row를 저장하기 위한 list
+        place_list = []
+
+        # 한 페이지의 place 리스트 무한 반복하여 클릭
+        while(True):
+            time.sleep(1)
+            # searchiframe으로 전환
+            driver.switch_to.default_content()
+            driver.switch_to.frame('searchIframe')
+
+            # 페이징내에서의 리스트 idx값
+            list_idx += 1
+
+            # 한 페이지에 클릭할 place가 없을 경우 break를 하기 위한 체크 값
+            try:
+                list_ldx_chk = bool(driver.find_element_by_css_selector(
+                    '#_pcmap_list_scroll_container ul li:nth-child(' + str(list_idx) + ')'))
+                print('list 여부 : ' + str(list_ldx_chk))
+
+                # 클락할 place 리스트가 있을 경우
+                if list_ldx_chk:
+
+                    # 광고 list 제외하기 위한 체크 값
+                    advertis_chk = bool(driver.find_elements_by_css_selector(
+                        '#_pcmap_list_scroll_container ul li:not(._293u6):nth-child(' + str(list_idx) + ')'))
+
+                    # 광고 list에 붙는 class명이 없을 경우(광고가 아닐 경우)
+                    if advertis_chk:
+
+                        print('광고가 아닌지 여부 : ' + str(advertis_chk))
+
+                        # list li의 idx값 1씩 증가
+                        # 광고일 경우엔 증가 x
+                        i += 1
+                        print(
+                            '=================================================================')
+                        print('===============================' +
+                              str(i) + '번 =============================')
+
+                        # place list 클릭해서 detailpage open
+                        content = driver.find_element_by_css_selector(
+                            '#_pcmap_list_scroll_container ul li:nth-child(' + str(list_idx) + ') ._3ZU00 ._3LMxZ:first-child')
+                        content.click()
+                        time.sleep(3)
+
+                        # place 이름 클릭하면 detailpage iframe으로 driver html 전환
+                        driver.switch_to.default_content()
+                        driver.switch_to.frame('entryIframe')
+
+                        # detail page의 html
+                        html = driver.page_source
+                        soup = BeautifulSoup(html, "html.parser")
+
+                        # ========== place 정보 추출 ==============
+
+                        # place 번호
+                        # place_list += i
+
+                        # place 이름
+                        place_name = soup.select_one('#_title ._3XamX').text
+                        print('매장명 : ' + place_name)
+                        place_list += [place_name]
+
+                        # place 위치
+                        place_address = soup.select_one(
+                            '.place_section_content ._1aj6- ._1h3B_ ._2yqUQ').text
+                        print('주소 : ' + place_address)
+                        place_list += place_address
+
+                        # place 전화번호
+                        try:
+                            place_tel = soup.select_one(
+                                '.place_section_content ._3xPmJ ._1h3B_ ._3ZA0S').text
+                            print('전화번호 : ' + place_tel)
+                            place_list += place_tel
+                        except Exception as e:
+                            place_tel = ''
+                            print('전화번호 : ' + place_tel)
+                            place_list += [place_tel]
+
+                        # place 이용정보
+                        try:
+
+                            place_info = soup.select_one(
+                                '.place_section_content ._6aUG7 ._1M_Iz._32wnh + ._1M_Iz ._1h3B_').text
+                            print('이용정보 : ' + place_info)
+                            place_list += place_info
+                        except Exception as e:
+                            place_info = ""
+                            print('이용정보 :' + place_info)
+                            place_list += [place_info]
+
+                        # place 소개
+                        try:
+                            place_introduce_open = driver.find_element_by_css_selector(
+                                '.place_section_content ._3__3i ._1h3B_ .M_704')
+                            place_introduce_open.click()
+                            time.sleep(2)
+
+                            # 내용 더 보기를 클릭함으로 해당 클래스 안에 text가 더 노출되며 동시에 해당 클래스에 클래스명이 추가된다.
+                            # 추가된 클래스명은 html변수에 driver.page_source로 담겨있지 않기 때문에 새롭게 선언해준다.
+                            driver.switch_to.default_content()
+                            driver.switch_to.frame('entryIframe')
+                            # detail page의 html
+                            html = driver.page_source
+                            soup = BeautifulSoup(html, "html.parser")
+
+                            place_introduce = soup.select_one(
+                                '.place_section_content ._3__3i ._1h3B_ .M_704 .WoYOw').text
+                            print('소개 : ' + place_introduce)
+                            place_list += place_introduce
+                        except Exception as e:
+                            place_introduce = ""
+                            print('소개 : ' + place_introduce)
+                            place_list += [place_introduce]
+
+                        # place 운영시간
+                        try:
+                            place_introduce_open = driver.find_element_by_css_selector(
+                                '.place_section_content ._2KHqk ._1h3B_ ._2BDci')
+                            place_introduce_open.click()
+                            time.sleep(0.5)
+
+                            # 내용 더 보기를 클릭함으로 해당 클래스 안에 text가 더 노출되며 동시에 해당 클래스에 클래스명이 추가된다.
+                            # 추가된 클래스명은 html변수에 driver.page_source로 담겨있지 않기 때문에 새롭게 선언해준다.
+                            driver.switch_to.default_content()
+                            driver.switch_to.frame('entryIframe')
+                            # detail page의 html
+                            html = driver.page_source
+                            soup = BeautifulSoup(html, "html.parser")
+
+                            # 운영시간이 여러개로 입력되어 있을 경우
+                            try:
+                                place_time_lists = driver.find_elements_by_css_selector(
+                                    '.place_section_content ._2KHqk ._1h3B_ ._2BDci._1OkoP ._2ZP3j ._20pEw')
+
+                            # 운영시간이 힌개만 입력되어 있을 경우
+                            except Exception as e:
+                                place_time_lists = driver.find_elements_by_css_selector(
+                                    '.place_section_content ._2KHqk ._1h3B_ ._2BDci._1OkoP ._2ZP3j')
+                            place_time_Arr = []
+
+                            for place_time_list in place_time_lists:
+                                place_time = place_time_list.text
+                                print('영업시간 : ' + place_time)
+                                place_time_Arr += [place_time]
+
+                            place_list = [place_time_Arr]
+                        except Exception as e:
+                            place_time = ""
+                            print('영업시간 : ' + place_time)
+                            place_list += [place_time]
+
+                        # place image
+                        try:
+                            image_section = driver.find_element_by_css_selector(
+                                '.place_detail_wrapper ._2MDmw ._tab-menu:last-child')
+
+                            image_section.click()
+
+                        # 시간 지연
+                            time.sleep(2)
+
+                            images = driver.find_elements_by_css_selector(
+                                ".place_section ._3TiO6._1wsFm ._2OSze .place_thumb img")
+                            print(len(images))
+
+                        # 해당 place의 img 갯수
+                            count = 1
+                            imagesArr = []
+                            for image in images:
+                                time.sleep(0.5)
+                                path = 'C:\\Seungmin\\python\\crawling\\images\\ '
+                                # 이미지 url 저장
+                                imgUrl = image.get_attribute("src")
+                                print('이미지 ' + str(count) + ' : ' + imgUrl)
+                                # 이미지 url을 저장한 list
+                                imagesArr += [imgUrl]
+                                # 이미지 파일 저장
+                                # urllib.request.urlretrieve(imgUrl, path + str(i) + '_' + str(count) + ".jpg")
+                                count = count + 1
+
+                                place_list += [imagesArr]
+
+                        except Exception as e:
+                            print('이미지 : 없음')
+                            place_list += ''
+                        # =========== place 정보 추출 완료 ============
+
+                        # file.write(i + "," + place_name + "," + place_address + "," + place_tel + "," +
+                        #            place_introduce + "," + place_time_list + "," + place_info + "," + imgUrl + "\n")
+                        print(place_list)
+                        writer.writerow(place_list)
+
+                        time.sleep(3)
+
+                    # 광고일 경우 continue로 패스
+                    else:
+                        continue
+
+                    # while문으로 다시 진행
+                    # continue
+                    break
+            # 다음 선택할 list가 없을 경우 while 종료
+            except Exception as e:
+                break
+        break
+    except Exception as e:
+        print(e)
+
+
+driver.close
+file.close()
