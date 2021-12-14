f1570b84b99f0742aec234c18ef338203c228d62
seungmin1592 <tmdals1592@naver.com>
2021-12-14 23:43:20 +0900

    Spring practice
    스프링 핵심 원리 - 기본편

--- SpringBoot/hello-spring/hello-spring/src/main/java/hello/hellospring/SpringConfig.java
+++ SpringBoot/hello-spring/hello-spring/src/main/java/hello/hellospring/SpringConfig.java
@@ -1,19 +1,23 @@
 package hello.hellospring;
 
-import hello.hellospring.repository.JdbcMemberRepository;
-import hello.hellospring.repository.JdbcTemplateMemberRepository;
-import hello.hellospring.repository.MemberRepository;
-import hello.hellospring.repository.MemoryMemberRepository;
+import hello.hellospring.repository.*;
 import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.persistence.EntityManager;
 import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
 
-    private DataSource dataSource;
+    private EntityManager em;
+
+    @Autowired
+    public SpringConfig(EntityManager em) {
+        this.em = em;
+    }
 
     @Bean
     public MemberService memberService(){
@@ -25,7 +29,8 @@ public class SpringConfig {
 
         //return new MemoryMemberRepository();
         // return new JdbcMemberRepository(dataSource);
-        return new JdbcTemplateMemberRepository(dataSource);
+        // return new JdbcTemplateMemberRepository(dataSource);
+        return new JpaMemberRepository();
     }
 
 }
