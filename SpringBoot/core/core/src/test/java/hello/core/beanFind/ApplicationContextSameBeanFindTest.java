eb6174f20e4d56592207bb319a2b02fd2e51f96a
seungmin1592 <tmdals1592@naver.com>
2022-01-02 16:09:51 +0900

    Spring Practice

--- SpringBoot/core/core/src/test/java/hello/core/beanFind/ApplicationContextSameBeanFindTest.java
+++ SpringBoot/core/core/src/test/java/hello/core/beanFind/ApplicationContextSameBeanFindTest.java
@@ -21,6 +21,7 @@ public class ApplicationContextSameBeanFindTest {
 
     @Test
     @DisplayName("타입으로 조회시 같은 타입이 둘 이상 있으면, 중복 오류가 발생한다.")
+
     void findBeanByTypeDuplicate(){
         assertThrows(NoUniqueBeanDefinitionException.class, () -> ac.getBean(MemberRepository.class));
     }
