0758f546c3e2a5c9f0362b2e0bd85b4ab0ba5e6e
seungmin1592 <tmdals1592@naver.com>
2022-01-11 23:26:27 +0900

    Spring Practice

--- SpringBoot/core/core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ SpringBoot/core/core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -22,4 +22,9 @@ public class OrderServiceImpl implements OrderService {
 
         return new Order(memberid, itemName, itemPrice, discountPrice);
     }
+    
+    // 테스트 용도
+    public MemberRepository getMemberRepository() {
+        return memberRepository;
+    }
 }
